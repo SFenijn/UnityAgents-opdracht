a09021d62d7b77d620a8f832be23efbd43158dd3
SFenijn <stijn.fenijn@student.hu.nl>
2019-11-29 18:13:03 +0100

    opdracht 3

---
+++ Assets/Scripts/Sence/Touch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class Touch : Sense
+{
+    private Color color;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Aspect aspect = other.GetComponent<Aspect>();
+        if (aspect != null)
+        {
+            //Check the aspect
+            if (aspect.aspectType != aspectName)
+            {
+                // GetComponent<Renderer>(Color.cyan);
+
+                print("Enemy Touch Detected");
+                
+            }
+        }
+    }
+}
